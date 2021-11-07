cf70798a44d917b56c80d5d1b483e1f1c1c26075
MohammadAljadayh <mjedayh@gmail.com>
2021-11-07 16:47:37 +0200

    add config to test

--- src/models/index.js
+++ src/models/index.js
@@ -2,21 +2,24 @@
 
 require('dotenv').config();
 
-const POSTGRES_URI = process.env.DATABASE_URL;
+
+const POSTGRES_URI = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
 
 const { Sequelize, DataTypes } = require('sequelize');
 const Users = require('./users-model')
 
 
-// const sequelize = new Sequelize(POSTGRES_URI, {
-//   dialectOptions: {
-//       ssl: {
-//           require: true,
-//           rejectUnauthorized: false,
-//       }
-//   }
-// });
-const sequelize = new Sequelize(POSTGRES_URI);
+let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+} : {};
+let sequelize = new Sequelize(POSTGRES_URI, sequelizeOptions);
+
+// const sequelize = new Sequelize(POSTGRES_URI);
 
 
 module.exports = {
